891d042cf73caebadc5ad0c463b314579fd5b678
ssdv1 <160284219+ssdv1@users.noreply.github.com>
2024-04-22 17:23:56 -0400

    Update settings.py

--- UML/UML/settings.py
+++ UML/UML/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = "UML.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'university',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
     }
 }
+
 
 
 # Password validation
