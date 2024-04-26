19482df1f8ab29ee18b35daa32130057241bb692
ssdv1 <160284219+ssdv1@users.noreply.github.com>
2024-04-26 14:32:36 -0400

    Update urls.py

--- UML/myProject/urls.py
+++ UML/myProject/urls.py
@@ -1,21 +1,13 @@
 from . import views
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("university", views.uni_home, name="home"),
-    path("professor/", views.professor, name="professor"),
-    path(
-        "professor_valid/<str:professor_id>/",
-        views.professor_valid,
-        name="professor_valid",
-    ),
-    path("student/", views.student, name="student"),
-    path(
-        "department_semester_form/",
-        views.department_semester_form,
-        name="department_semester_form",
-    ),
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path('professor/', views.professor, name='professor'),
+    path('professor_valid/<str:professor_id>/', views.professor_valid, name='professor_valid'),
+    path('student/', views.student, name='student'),
+    path('department_semester_form/',views.department_semester_form,name='department_semester_form')
 ]
+
